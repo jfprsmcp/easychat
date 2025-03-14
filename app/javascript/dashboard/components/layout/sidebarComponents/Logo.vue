07b92b9d3d058892a0cf305f43f2da33ac64b72a
eduardo <eduardo.rodriguez.info@gmail.com>
2025-03-13 21:34:18 -0400

    agregando animaciones, logo y actualizando colores en la landing

--- app/javascript/dashboard/components/layout/sidebarComponents/Logo.vue
+++ app/javascript/dashboard/components/layout/sidebarComponents/Logo.vue
@@ -27,7 +27,13 @@ export default {
 <template>
   <div class="w-8 h-8">
     <router-link :to="dashboardPath" replace>
-      <img :src="source" :alt="name" />
+      <img :src="source" :alt="name" class="shadow-filter-logo"/>
     </router-link>
   </div>
 </template>
+
+<style>
+.shadow-filter-logo{
+  filter: drop-shadow(0px 0px 2px #22223455);
+}
+</style>
