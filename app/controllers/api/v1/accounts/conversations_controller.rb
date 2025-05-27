5cc5ed39ead44c9b94ec99ebd6c89f797bd505ba
eduardo <eduardo.rodriguez.info@gmail.com>
2025-05-27 19:16:06 -0400

    agregando campos adicionales para actualizar una conversation, back

--- app/controllers/api/v1/accounts/conversations_controller.rb
+++ app/controllers/api/v1/accounts/conversations_controller.rb
@@ -37,9 +37,7 @@ class Api::V1::Accounts::ConversationsController < Api::V1::Accounts::BaseContro
   end
 
   def update
-    if(params[:kanban_states_id]=='null')
-      params[:kanban_states_id] = nil
-    end
+    params[:kanban_states_id] = nil if params[:kanban_states_id] == 'null'
     @conversation.update!(permitted_update_params)
   end
 
@@ -123,7 +121,7 @@ class Api::V1::Accounts::ConversationsController < Api::V1::Accounts::BaseContro
 
   def permitted_update_params
     # TODO: Move the other conversation attributes to this method and remove specific endpoints for each attribute
-    params.permit(:priority,:kanban_states_id)
+    params.permit(:priority,:kanban_states_id,:score,:justification,:conversation_sentiment_id)
   end
 
   def update_last_seen_on_conversation(last_seen_at, update_assignee)
