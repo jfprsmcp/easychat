9f68f0c22fe8f2440135dca94dd5237953fa2f5d
eduardo <eduardo.rodriguez.info@gmail.com>
2025-07-18 11:33:04 -0400

    agregando campo prompt en la creacion agentes bots en super_admin, frontback

--- app/controllers/super_admin/agent_bots_controller.rb
+++ app/controllers/super_admin/agent_bots_controller.rb
@@ -33,11 +33,11 @@ class SuperAdmin::AgentBotsController < SuperAdmin::ApplicationController
   # empty values into nil values. It uses other APIs such as `resource_class`
   # and `dashboard`:
   #
-  # def resource_params
-  #   params.require(resource_class.model_name.param_key).
-  #     permit(dashboard.permitted_attributes).
-  #     transform_values { |value| value == "" ? nil : value }
-  # end
+  def resource_params
+    params.require(resource_class.model_name.param_key).
+      permit(dashboard.permitted_attributes + [:prompt]).
+      transform_values { |value| value == "" ? nil : value }
+  end
 
   # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
   # for more information
